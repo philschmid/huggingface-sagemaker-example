251644659ef62f104e026c559116c06bd18b48ea
Philipp Schmid <schmidphilipp1995@gmail.com>
2020-12-22 15:33:22 +0100

    added custom train loop example

--- 02_local_huggingface_sagemaker_custom_train/src/train.py
+++ 02_local_huggingface_sagemaker_custom_train/src/train.py
@@ -1,11 +1,15 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer,AutoModelForSequenceClassification, Trainer, TrainingArguments
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 import random
 import logging
 import sys
 import argparse
 import os
+from torch.utils.data import DataLoader
+from transformers import AdamW, AutoTokenizer,AutoModelForSequenceClassification
+import torch
+from tqdm import tqdm
+import numpy as np
 
 if __name__ =='__main__':
 
@@ -15,8 +19,7 @@ if __name__ =='__main__':
     parser.add_argument('--epochs', type=int, default=3)
     parser.add_argument('--train-batch-size', type=int, default=32)
     parser.add_argument('--eval-batch-size', type=int, default=64)
-    parser.add_argument('--warmup_steps', type=int, default=500)
-    parser.add_argument('--model_name', type=str)
+    parser.add_argument('--model-name', type=str)
     parser.add_argument('--tokenizer', type=str)
 
     # Data, model, and output directories
@@ -29,9 +32,7 @@ if __name__ =='__main__':
     args, _ = parser.parse_known_args()
 
     # Set up logging
-    
     logger = logging.getLogger(__name__)
-
     logging.basicConfig(level=logging.getLevelName('INFO'), handlers=[logging.StreamHandler(sys.stdout)],
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -69,51 +70,90 @@ if __name__ =='__main__':
     test_dataset.rename_column_("label", "labels")
     test_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
 
-    # compute metrics function for binary classification
-    def compute_metrics(pred):
-        labels = pred.label_ids
-        preds = pred.predictions.argmax(-1)
-        precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='binary')
-        acc = accuracy_score(labels, preds)
-        return {
-            'accuracy': acc,
-            'f1': f1,
-            'precision': precision,
-            'recall': recall
-        }
+    # creates dataloader
+    train_loader = DataLoader(train_dataset, batch_size=args.train_batch_size, shuffle=True)
+    eval_dataloader = DataLoader(test_dataset, batch_size=args.eval_batch_size, shuffle=True)
 
-    # define training args 
-    training_args = TrainingArguments(
-        output_dir=args.model_dir,
-        num_train_epochs=args.epochs,
-        per_device_train_batch_size=args.train_batch_size,
-        per_device_eval_batch_size=args.eval_batch_size,
-        warmup_steps=args.warmup_steps,
-        evaluation_strategy='epoch',
-        logging_dir=f'{args.output_data_dir}/logs',
-    )
+    # get device
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    
+    # puts model on device
+    model.to(device)
+    
+    # puts model in train mode
+    model.train()
 
-    # create Trainer instance
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        compute_metrics=compute_metrics,
-        train_dataset=train_dataset,
-        eval_dataset=test_dataset
-    )
+    # optimizer
+    optim = AdamW(model.parameters(), lr=5e-5)
 
-    # train model
-    trainer.train()
+    # train loop
+    for epoch in tqdm(args.epochs, desc="training"):
+        for batch in train_loader:
+            optim.zero_grad()
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+            loss = outputs[0]
+            loss.backward()
+            optim.step()
 
-    # evaluate model
-    eval_result = trainer.evaluate(eval_dataset=test_dataset)
+    # puts model into evaluation mode
+    model.eval()
+
+    # custom compute metrics for evaluation
+    def compute_metrics(preds, labels):
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='binary')
+    acc = accuracy_score(labels, preds)
+    return {
+        'accuracy': acc,
+        'f1': f1,
+        'precision': precision,
+        'recall': recall
+    }
+
+    logger.info("***** Running evaluation *****")
+    logger.info("  Num examples = %d", len(test_dataset))
+
+
+    eval_loss = 0.0
+    nb_eval_steps = 0
+    preds = None
+    out_label_ids = None
+    
+    # evaluation loop
+    for batch in tqdm(eval_dataloader, desc="Evaluating"):
+        model.eval()
+        with torch.no_grad():
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['label'].to(device)
+            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+            tmp_eval_loss, logits = outputs[:2]
+
+            eval_loss += tmp_eval_loss.mean().item()
+        nb_eval_steps += 1
+        if preds is None:
+            preds = logits.detach().cpu().numpy()
+            out_label_ids = labels.detach().cpu().numpy()
+        else:
+            preds = np.append(preds, logits.detach().cpu().numpy(), axis=0)
+            out_label_ids = np.append(out_label_ids, labels.detach().cpu().numpy(), axis=0)
+
+
+    # compute evluation metric
+    eval_loss = eval_loss / nb_eval_steps
+    preds = np.argmax(preds, axis=1)
+    result = compute_metrics(preds, out_label_ids)
 
     # writes eval result to file which can be accessed later in s3 ouput
     with open(os.path.join(args.output_data_dir, "eval_results.txt"), "w") as writer:
-        print(f"***** Eval results *****")
-        for key, value in sorted(eval_result.items()):
-            writer.write(f"{key} = {value}\n")
-
-     # Saves the model to s3
-    trainer.save_model(args.model_dir) 
+        logger.info("***** Eval results  *****")
+        for key in sorted(result.keys()):
+            logger.info("  %s = %s", key, str(result[key]))
+            writer.write("%s = %s\n" % (key, str(result[key])))
+            
+            
+    # Saves the model to s3
+    model.save_pretrained(args.model_dir) 
 
