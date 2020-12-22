bf1e2232ba8100c6c1086b9410d5d9da4274995d
Philipp Schmid <schmidphilipp1995@gmail.com>
2020-12-22 17:44:16 +0100

    reworked all notebooks to be used in sagemaker studio

--- 03_huggingface_sagemaker_trainer_with_s3/src/train.py
+++ 03_huggingface_sagemaker_trainer_with_s3/src/train.py
@@ -1,11 +1,11 @@
-from datasets import load_dataset
-from transformers import AutoTokenizer,AutoModelForSequenceClassification, Trainer, TrainingArguments
+from transformers import AutoModelForSequenceClassification, Trainer, TrainingArguments
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 import random
 import logging
 import sys
 import argparse
 import os
+import torch
 
 if __name__ =='__main__':
 
@@ -17,57 +17,22 @@ if __name__ =='__main__':
     parser.add_argument('--eval-batch-size', type=int, default=64)
     parser.add_argument('--warmup_steps', type=int, default=500)
     parser.add_argument('--model_name', type=str)
-    parser.add_argument('--tokenizer', type=str)
 
     # Data, model, and output directories
     parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
     parser.add_argument('--n_gpus', type=str, default=os.environ['SM_NUM_GPUS'])
-    # parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
-    # parser.add_argument('--test', type=str, default=os.environ['SM_CHANNEL_TEST'])
+    parser.add_argument('--training_dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
+    parser.add_argument('--test_dir', type=str, default=os.environ['SM_CHANNEL_TEST'])
 
     args, _ = parser.parse_known_args()
 
-    # Set up logging
+    # Get datasets
+    train_dataset  = torch.load(os.path.join(args.training_dir, 'train_dataset.pt'))
+    test_dataset  = torch.load(os.path.join(args.test_dir, 'test_dataset.pt'))
     
-    logger = logging.getLogger(__name__)
-
-    logging.basicConfig(level=logging.getLevelName('INFO'), handlers=[logging.StreamHandler(sys.stdout)],
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    # load dataset
-    dataset = load_dataset('imdb')
-
-    # download model and tokenizer from model hub
-    model = AutoModelForSequenceClassification.from_pretrained(args.model_name)
-    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
-
-    #helper tokenizer function
-    def tokenize(batch):
-        return tokenizer(batch['text'], padding='max_length', truncation=True)
-
-    # load dataset
-    train_dataset, test_dataset = load_dataset('imdb', split=['train', 'test'])
-    test_dataset = test_dataset.shuffle().select(range(10000)) # smaller the size for test dataset to 10k 
-
-    # sample a to small dataset for training
-    train_dataset = train_dataset.shuffle().select(range(1000)) # smaller the size for test dataset to 10k 
-    test_dataset = test_dataset.shuffle().select(range(50)) # smaller the size for test dataset to 10k 
-
-
-    # tokenize dataset
-    train_dataset = train_dataset.map(tokenize, batched=True, batch_size=len(train_dataset))
-    test_dataset = test_dataset.map(tokenize, batched=True, batch_size=len(test_dataset))
-
-    # Log a few random samples from the training set:
-    for index in random.sample(range(len(train_dataset)), 3):
-        logger.info(f"Sample {index} of the training set: {train_dataset[index]}.")
-
-    # set format for pytorch
-    train_dataset.rename_column_("label", "labels")
-    train_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
-    test_dataset.rename_column_("label", "labels")
-    test_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
+    print(len(train_dataset))
+    print(len(test_dataset))
 
     # compute metrics function for binary classification
     def compute_metrics(pred):
@@ -108,7 +73,7 @@ if __name__ =='__main__':
     # evaluate model
     eval_result = trainer.evaluate(eval_dataset=test_dataset)
 
-    # writes eval result to file which can be accessed later
+    # writes eval result to file which can be accessed later in s3 ouput
     with open(os.path.join(args.output_data_dir, "eval_results.txt"), "w") as writer:
         print(f"***** Eval results *****")
         for key, value in sorted(eval_result.items()):
--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # huggingface-sagemaker-example
 
-This Repository contains multiple examples how to use the transformers and datasets library from HuggingFace.
+This Repository contains multiple examples how to use the transformers and datasets library from HuggingFace. All Notebooks can be run locally or with in AWS Sagemaker Studio.
 
 **structure**
 
 Each folder starting with `0X_..` contains an specific sagemaker example. Each example contains a jupyter notebooke `sagemaker-example.ipynb` and a `src/` folder. The `sagemaker-example` is a jupyter notebook which is used to train transformers and datasets on AWS Sagemaker. The `src/` folder contains the `train.py`, our training script and `requirements.txt` for additional dependencies.
 
 `notebook_template/` contains the original colab notebook, which is the baseline for the sagemaker example.
-
 
 # How to use Sagemaker Data Studio
